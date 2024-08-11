f129e0796e096f64dd08f7698b35f15c943425ab
elepedus <ed.lepedus@googlemail.com>
2024-08-11 18:57:50 +0100

    chore: add `ex_doc` to deps

--- mix.exs
+++ mix.exs
@@ -38,8 +38,7 @@ defmodule KinoUserPresence.MixProject do
   defp deps do
     [
       {:kino, "~> 0.13.2"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
