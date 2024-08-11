632263a160820f467f6c7028020f126c4a85cb16
elepedus <ed.lepedus@googlemail.com>
2024-08-11 18:10:58 +0100

    chore: add name & description to `mix.exs`

--- mix.exs
+++ mix.exs
@@ -7,8 +7,16 @@ defmodule KinoUserPresence.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      name: "Kino User Presence",
       deps: deps()
     ]
+  end
+
+  def description() do
+    """
+    Provides user presence tracking functionality for Livebook applications.
+    """
   end
 
   # Run "mix help compile.app" to learn about applications.
