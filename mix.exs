ff1cc0f3234a06f8cb73f83a15baabbd2b121ce9
elepedus <ed.lepedus@googlemail.com>
2024-08-11 18:14:32 +0100

    chore: add `Kino` to deps

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule KinoUserPresence.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:kino, "~> 0.13.2"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
